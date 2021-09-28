9640952347b1a0675dfa6e09efed304c2d8da67f
Miguel Marte <40346318+miguelmarte@users.noreply.github.com>
2021-09-28 19:07:52 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $mysqli=new mysqli('localhost','root','','platano_play');
+    $mysqli=new mysqli('us-cdbr-east-04.cleardb.com','bf2a1e2e7ff367','a6bdb32f','heroku_29d6cdd65902293');
     if($mysqli->connect_errno):
         echo "Error al conectarse con MySQL debido al error ".$mysqli->connect_error;
     endif;
